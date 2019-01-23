b8a6d4519eb69336bcec0eddb77ae34a6581ce7b
Saphira Flug <flug@atb-bremen.de>
2019-01-23 19:18:13 +0100

    still cleaning

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/wizard/Messages.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/wizard/Messages.java
@@ -4,26 +4,21 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "de.atb.typhondl.xtext.ui.wizard.messages"; //$NON-NLS-1$
-
+	
 	public static String HelloWorldProject_Label;
 	public static String HelloWorldProject_Description;
-	
 	public static String WizardLoadModel_nameLabel;
 	public static String WizardLoadModel_useModel;
 	public static String WizardLoadModel_browseLabel;
 	public static String WizardLoadModel_fileLocationEmpty;
 	public static String WizardLoadModel_locationError;
 	public static String WizardLoadModel_directoryLabel;
-	
 	public static String TemplateNewProjectWizard_title_suffix;
 	public static String TemplateNewProjectWizard_create_new_prefix;
 	public static String TemplateNewProjectWizard_create_new_suffix;
 	public static String TemplateNewProjectWizard_title;
-	
 	public static String WizardSelectionPage_title_suffix;
 	public static String WizardSelectionPage_description;
-	
-	
 	
 	static {
 	// initialize resource bundle
