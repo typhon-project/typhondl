0c46495105f79fa35be831feac69aa33eb4736e9
Saphira Flug <flug@atb-bremen.de>
2019-03-04 00:16:54 +0100

    change to Java11, create wizard, idea for generating scripts

---
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/wizard/Messages.java
@@ -0,0 +1,39 @@
+package de.atb.typhondl.xtext.ui.wizard;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "de.atb.typhondl.xtext.ui.wizard.messages"; //$NON-NLS-1$
+	
+	public static String HelloWorldProject_Label;
+	public static String HelloWorldProject_Description;
+	public static String WizardLoadModel_nameLabel;
+	public static String WizardLoadModel_useModel;
+	public static String WizardLoadModel_browseLabel;
+	public static String WizardLoadModel_fileLocationEmpty;
+	public static String WizardLoadModel_locationError;
+	public static String WizardLoadModel_directoryLabel;
+	public static String WizardLoadModel_existError;
+	public static String WizardLoadModel_fileError;
+	public static String WizardLoadModel_wrongExtension;
+	public static String TemplateNewProjectWizard_title_suffix;
+	public static String TemplateNewProjectWizard_create_new_prefix;
+	public static String TemplateNewProjectWizard_create_new_suffix;
+	public static String TemplateNewProjectWizard_title;
+	public static String WizardSelectionPage_title_suffix;
+	public static String WizardSelectionPage_description;
+	public static String DockerCompose_Label;
+	public static String DockerCompose_Description;
+	public static String Kubernetes_Label;
+	public static String Kubernetes_Description;
+	public static String EmptyProject_Label;
+	public static String EmptyProject_Description;
+	
+	static {
+	// initialize resource bundle
+	NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
