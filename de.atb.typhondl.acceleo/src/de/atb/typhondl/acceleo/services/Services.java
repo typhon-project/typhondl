846e2df6e7c7ea967afbfd1d51f7944b332a1fa3
Saphira Flug <flug@atb-bremen.de>
2019-04-11 15:54:59 +0200

    WIP start generation via menu

--- de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/services/Services.java
+++ de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/services/Services.java
@@ -1,16 +1,44 @@
 package de.atb.typhondl.acceleo.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+
 import com.google.inject.Injector;
+
+import de.atb.typhondl.acceleo.main.Generate;
 import de.atb.typhondl.xtext.TyphonDLStandaloneSetup;
 import de.atb.typhondl.xtext.typhonDL.DeploymentModel;
 
 public class Services {
 
+	public static void generateDeployment(String pathToXTextModel, String folder) {
+			System.out.println("Generate from template...");
+			try {
+				Generate generator = new Generate(loadXtextModel(pathToXTextModel), new File(folder), new ArrayList<String>());
+				generator.doGenerate(new BasicMonitor());
+				System.out.println("Generated!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
 	public static DeploymentModel loadXtextModel(String pathToXTextModel) {
 		Injector injector = new TyphonDLStandaloneSetup().createInjector();
+		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+		URI uri = URI.createURI(pathToXTextModel);
+		Resource resource = resourceSet.getResource(uri, true);
+		DeploymentModel deploymentModel = (DeploymentModel) resource.getContents().get(0);
 		
 		
-		
-		return null;
+		return deploymentModel;
 	}
 }
--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
@@ -1,14 +1,49 @@
 package de.atb.typhondl.xtext.ui.handlers;
 
+import java.io.File;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.atb.typhondl.acceleo.services.Services;
 
 public class GenerateScriptsHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+
+		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelectionChecked(event);
+		Object object = selection.getFirstElement();
+
+		if (object instanceof IFile) {
+			IFile file = (IFile) object;
+			String pathToXTextModel = file.getFullPath().toString();
+			File folder = new File(file.getLocation().toString().replace("." + file.getFileExtension(), "")
+					+ File.separator + "generated"); // TODO
+			Services.generateDeployment(pathToXTextModel, folder.getAbsolutePath());
+
+			for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+				try {
+					project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(), "UI", "Deployment Scripts were generated");
 		return null;
 	}
 
