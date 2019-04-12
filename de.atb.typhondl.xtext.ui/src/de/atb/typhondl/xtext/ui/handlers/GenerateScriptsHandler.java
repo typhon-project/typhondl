7fa8335f23a9ad6d4201c75fd6a97dbf21062508
Saphira Flug <flug@atb-bremen.de>
2019-04-12 15:18:09 +0200

    WIP generate as eclipse project

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
@@ -1,5 +1,6 @@
 package de.atb.typhondl.xtext.ui.handlers;
 
+
 import java.io.File;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -8,6 +9,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -25,16 +27,19 @@ public class GenerateScriptsHandler extends AbstractHandler {
 
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelectionChecked(event);
 		Object object = selection.getFirstElement();
-
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot(); //doesnt have to be workspace, but folder of modeling project
 		if (object instanceof IFile) {
+			//IFile ifile = (IFile) object;
 			IFile file = (IFile) object;
-			String pathToXTextModel = file.getFullPath().toString();
-			String modelName = file.getName().replace("." + file.getFileExtension(), "");
-			File folder = new File(ResourcesPlugin.getWorkspace().getRoot()
-					+ File.separator + modelName); // TODO
-			Services.generateDeployment(pathToXTextModel, folder.getAbsolutePath());
-
-			for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+			File tempFile = new File(root + File.separator);
+			String fileAbsolutPath = tempFile.getAbsolutePath();
+			System.out.println("absolutPath = " + fileAbsolutPath); 
+			String folder = tempFile.getAbsolutePath(); //TODO really not nice, its root, but that doesn't work
+			System.out.println("folder = " + folder);
+			System.out.println("Services.generateDeployment(" + file.getFullPath().toOSString()+ ", " + folder + ")");
+			Services.generateDeployment(file.getFullPath().toString(), folder);
+			for (IProject project : root.getProjects()) { //TODO
+				System.out.println("projectName: " + project.getName());
 				try {
 					project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
 				} catch (CoreException e) {
@@ -45,6 +50,7 @@ public class GenerateScriptsHandler extends AbstractHandler {
 
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(window.getShell(), "UI", "Deployment Scripts were generated");
+		System.out.println("numberOfProjects in " + root + " :" + root.getProjects().length);
 		return null;
 	}
 
