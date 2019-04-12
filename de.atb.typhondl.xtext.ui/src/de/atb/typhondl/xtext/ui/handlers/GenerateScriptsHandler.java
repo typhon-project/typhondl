4545ce0a8b0c2dbbf7804eca610d6e753a836dc5
Saphira Flug <flug@atb-bremen.de>
2019-04-12 15:45:26 +0200

    WIP added project natures

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
@@ -1,6 +1,5 @@
 package de.atb.typhondl.xtext.ui.handlers;
 
-
 import java.io.File;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -8,15 +7,21 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.dialogs.IOverwriteQuery;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.wizards.datatransfer.ImportOperation;
 
 import de.atb.typhondl.acceleo.services.Services;
 
@@ -27,21 +32,32 @@ public class GenerateScriptsHandler extends AbstractHandler {
 
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelectionChecked(event);
 		Object object = selection.getFirstElement();
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot(); //doesnt have to be workspace, but folder of modeling project
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
 		if (object instanceof IFile) {
-			//IFile ifile = (IFile) object;
 			IFile file = (IFile) object;
-			File tempFile = new File(root + File.separator);
-			String fileAbsolutPath = tempFile.getAbsolutePath();
-			System.out.println("absolutPath = " + fileAbsolutPath); 
-			String folder = tempFile.getAbsolutePath(); //TODO really not nice, its root, but that doesn't work
-			System.out.println("folder = " + folder);
-			System.out.println("Services.generateDeployment(" + file.getFullPath().toOSString()+ ", " + folder + ")");
+			String folder = new File(root + File.separator).getAbsolutePath();
+
 			Services.generateDeployment(file.getFullPath().toString(), folder);
-			for (IProject project : root.getProjects()) { //TODO
-				System.out.println("projectName: " + project.getName());
+
+			String projectName = "deployment";
+			IProgressMonitor progressMonitor = new NullProgressMonitor();
+			IProject project = root.getProject(projectName);
+			IProjectDescription description = workspace.newProjectDescription(projectName);
+			String[] natures = {JavaCore.NATURE_ID, "org.eclipse.m2e.core.maven2Nature"};
+			description.setNatureIds(natures);
+			System.out.println("project: " + (project == null) + ", " + project.getName());
+			try {
+				project.create(progressMonitor);
+				project.open(progressMonitor);
+			} catch (CoreException e1) {
+				e1.printStackTrace();
+			}
+			
+			for (IProject iproject : root.getProjects()) { // TODO
+				System.out.println("projectName: " + iproject.getName());
 				try {
-					project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+					iproject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
 				} catch (CoreException e) {
 					e.printStackTrace();
 				}
