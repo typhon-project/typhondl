73c480ba864b2039612c5160e93c6188aacd431f
Saphira Flug <flug@atb-bremen.de>
2019-04-24 15:33:42 +0200

    not working, don't know why

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/handlers/GenerateScriptsHandler.java
@@ -7,21 +7,17 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.dialogs.IOverwriteQuery;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.wizards.datatransfer.ImportOperation;
+
 import de.atb.typhondl.acceleo.services.Services;
 
 public class GenerateScriptsHandler extends AbstractHandler {
@@ -35,26 +31,13 @@ public class GenerateScriptsHandler extends AbstractHandler {
 		IWorkspaceRoot root = workspace.getRoot();
 		if (object instanceof IFile) {
 			IFile file = (IFile) object;
-			String folder = new File(root + File.separator).getAbsolutePath();
-
-			Services.generateDeployment(file.getFullPath().toString(), folder);
-
-			String projectName = "deployment";
-			IProgressMonitor progressMonitor = new NullProgressMonitor();
-			IProject project = root.getProject(projectName);
-			IProjectDescription description = workspace.newProjectDescription(projectName);
-			String[] natures = {JavaCore.NATURE_ID, "org.eclipse.m2e.core.maven2Nature"};
-			description.setNatureIds(natures);
-			System.out.println("project: " + (project == null) + ", " + project.getName());
-			try {
-				project.create(progressMonitor);
-				project.open(progressMonitor);
-			} catch (CoreException e1) {
-				e1.printStackTrace();
-			}
-			
+			File folder = new File(file.getLocation().toOSString().replace("." + file.getFileExtension(), "")
+					+ File.separator + "output");
+			System.out.println("Outputfolder: " + folder);
+			System.out.println("inputModel: " + file.getFullPath().toString());
+			Services.generateDeployment(file.getFullPath().toString(), folder.getAbsolutePath());
+			System.out.println("after Generation");
 			for (IProject iproject : root.getProjects()) { // TODO
-				System.out.println("projectName: " + iproject.getName());
 				try {
 					iproject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
 				} catch (CoreException e) {
@@ -65,8 +48,8 @@ public class GenerateScriptsHandler extends AbstractHandler {
 
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(window.getShell(), "UI", "Deployment Scripts were generated");
-		System.out.println("numberOfProjects in " + root + " :" + root.getProjects().length);
 		return null;
+
 	}
 
 }
