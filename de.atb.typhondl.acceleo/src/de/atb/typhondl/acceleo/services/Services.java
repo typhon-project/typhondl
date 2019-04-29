f9e23e12be27f962ea743cf360425ad7128f633d
Saphira Flug <flug@atb-bremen.de>
2019-04-29 12:21:23 +0200

    save Model as xmi

--- de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/services/Services.java
+++ de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/services/Services.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
@@ -15,18 +17,46 @@ import de.atb.typhondl.xtext.typhonDL.DeploymentModel;
 public class Services {
 
 	public static void generateDeployment(String pathToXTextModel, String folder) {
-			System.out.println("Generate from template...");
-			try {
-				new Generate(loadXtextModel(pathToXTextModel), new File(folder), new ArrayList<String>()).doGenerate(new BasicMonitor());
-				System.out.println("Generated!");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		System.out.println("Generate from template...");
+		try {
+			new Generate(loadXtextModel(pathToXTextModel, folder), new File(folder), new ArrayList<String>())
+					.doGenerate(new BasicMonitor());
+			System.out.println("Generated!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static DeploymentModel loadXtextModel(String pathToXTextModel, String folder) {
+		/* TODO TYP-45
+		 * https://stackoverflow.com/questions/35839786/xtext-export-model-as-xmi-xml#35885943
+		 *
+		 * Inside Eclipse IDE never use MyLanguageStandaloneSetup, the instance of an
+		 * injector MUST be accessed via an Activator of a UI plugin:
+		 * MyLanguageActivator.getInstance().getInjector(MyLanguageActivator.
+		 * COM_MYCOMPANY_MYLANGUAGE).
+		 * 
+		 * Calling of MyLanguageStandaloneSetup.createInjectorAndDoEMFRegistration will
+		 * create a new instance of an Injector that is different from used by Eclipse.
+		 * Also it can break the state of EMF registries.
+		 */
+		XtextResourceSet resourceSet = new TyphonDLStandaloneSetup().createInjector()
+				.getInstance(XtextResourceSet.class);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+		URI xtextModelURI = URI.createURI(pathToXTextModel);
+		saveModelAsXMI(xtextModelURI, folder, resourceSet);
+		return (DeploymentModel) resourceSet.getResource(xtextModelURI, true).getContents().get(0);
 	}
 	
-	public static DeploymentModel loadXtextModel(String pathToXTextModel) {
-		XtextResourceSet resourceSet = new TyphonDLStandaloneSetup().createInjector().getInstance(XtextResourceSet.class);
-		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-		return (DeploymentModel) resourceSet.getResource(URI.createURI(pathToXTextModel), true).getContents().get(0);
+	public static void saveModelAsXMI(URI xtextModelURI, String pathToTargetFolder, XtextResourceSet resourceSet) {
+		Resource xtextResource = resourceSet.getResource(xtextModelURI, true);
+		EcoreUtil.resolveAll(xtextResource);
+		Resource xmiResource = resourceSet.createResource(URI.createURI(pathToTargetFolder));
+	    xmiResource.getContents().add(xtextResource.getContents().get(0));
+	    try {
+	        xmiResource.save(null);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 	}
 }
