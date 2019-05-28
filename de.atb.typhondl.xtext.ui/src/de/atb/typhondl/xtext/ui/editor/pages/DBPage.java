39e7e7d1233a2a47115b4b6cc7cf1a9f2e5eec6f
Saphira Flug <flug@atb-bremen.de>
2019-05-28 12:25:26 +0200

    saving image and key_value is possible

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/editor/pages/DBPage.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/editor/pages/DBPage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -40,6 +41,7 @@ public class DBPage extends EditorPage {
 		TyphonFieldEditor field3 = new TyphonFieldEditor(db.getName() + ".image", "image", parent);
 		field3.setStringValue(db.getImage().getValue());
 		field3.setPreferenceStore(preferenceStore);
+		field3.store(); //is needed in case model was changed in textual editor
 		fieldEditorList.add(field3);
 		for (Property property : db.getParameters()) {
 			createPropertyFields(property, parent);
@@ -55,6 +57,8 @@ public class DBPage extends EditorPage {
 					keyValue.getName(), parent);
 			field.setStringValue(keyValue.getValue());
 			field.setPreferenceStore(preferenceStore);
+			field.store();
+			System.out.println(field.getPreferenceName() + ":" + preferenceStore.getString(field.getPreferenceName()));
 			fieldEditorList.add(field);
 			break;
 		case "de.atb.typhondl.xtext.typhonDL.Key_ValueArray":
@@ -67,6 +71,7 @@ public class DBPage extends EditorPage {
 			}
 			field1.setStringValue(string);
 			field1.setPreferenceStore(preferenceStore);
+			field1.store();
 			fieldEditorList.add(field1);
 			break;
 		case "de.atb.typhondl.xtext.typhonDL.Key_ValueList":
@@ -90,6 +95,7 @@ public class DBPage extends EditorPage {
 						db.getName() + ".Key_ValueList." + property.getName() + "." + key, key, group);
 				field2.setStringValue(value);
 				field2.setPreferenceStore(preferenceStore);
+				field2.store();
 				fieldEditorList.add(field2);
 			}
 			break;
@@ -100,7 +106,6 @@ public class DBPage extends EditorPage {
 
 	@Override
 	public boolean performOk() {
-		boolean changed = false;
 		for (TyphonFieldEditor editor : fieldEditorList) {
 			String preference = preferenceStore.getString(editor.getPreferenceName());
 			String value = editor.getStringValue();
@@ -111,31 +116,34 @@ public class DBPage extends EditorPage {
 				String key = preference.substring(point + 1);
 				value = editor.getStringValue();
 				changedb(key, value);
-				changed = true;
+				changeResource();
 			}
-		}
-		if (changed) {
-			changeResource();
 		}
 		return super.performOk();
 	}
 
 	private void changedb(String key, String value) {
-		String type = key.indexOf('.') > 0 ? key.substring(0, key.indexOf('.')) : "";
-		String name = key.indexOf('.') > 0 ? key.substring(key.indexOf('.') + 1) : key;
-		List<Property> properties = db.getParameters();
-		System.out.println(properties.get(0).getName() + "=" + name);
-		Property prop = (Property) properties.stream().filter(property -> property.getName().equals(name));
-		System.out.println(prop.getName() + "=" + name);
-		switch (type) {
-		case "image":
+		if (key.equals("image")) {
 			db.getImage().setValue(value);
-			break;
+			return;
+		}
+		String type = key.indexOf('.') > 0 ? key.substring(0, key.indexOf('.')) : key;
+		String temp = key.indexOf('.') > 0 ? key.substring(key.indexOf('.') + 1) : key;
+		String name = temp.indexOf('.') > 0 ? temp.substring(0, temp.indexOf('.')) : temp;
+		Property prop = db.getParameters().stream().filter(property -> property.getName().equals(name)).findAny().get();
+		switch (type) {
 		case "Key_Value":
+			((Key_Value) prop).setValue(value);
 			break;
 		case "Key_ValueList":
+			Key_ValueList list = (Key_ValueList) prop;
+			System.out.println("not able to save this at the moment, pls wait for next version");
+			// TODO
 			break;
 		case "Key_ValueArray":
+			Key_ValueArray array = (Key_ValueArray) prop;
+			System.out.println("not able to save this at the moment, pls wait for next version");
+			// TODO
 			break;
 		default:
 			break;
