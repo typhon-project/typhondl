0969c7fdbe0191b93ff1bacde8b4d5bbd5569a10
Saphira Flug <flug@atb-bremen.de>
2019-06-25 14:36:23 +0200

    WIP main page

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/updateWizard/UpdateModelWizard.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/updateWizard/UpdateModelWizard.java
@@ -1,153 +1,52 @@
 package de.atb.typhondl.xtext.ui.updateWizard;
 
+import java.io.File;
 import java.net.URI;
 
-import org.eclipse.jface.dialogs.IDialogSettings;
-import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.jface.wizard.IWizardContainer;
-import org.eclipse.jface.wizard.IWizardPage;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.xtext.IGrammarAccess;
 import org.eclipse.xtext.ui.util.FileOpener;
+import org.eclipse.xtext.ui.wizard.template.AbstractFileTemplate;
+import org.eclipse.xtext.ui.wizard.template.NewFileWizardPrimaryPage;
 import org.eclipse.xtext.ui.wizard.template.TemplateLabelProvider;
+import org.eclipse.xtext.ui.wizard.template.TemplateNewFileWizard;
 
-public class UpdateModelWizard implements IWizard {
+@SuppressWarnings("restriction")
+public class UpdateModelWizard extends TemplateNewFileWizard {
 
-	public UpdateModelWizard(IGrammarAccess grammarAccess, TemplateLabelProvider labelProvider, FileOpener fileOpener) {
-		// TODO Auto-generated constructor stub
+	protected URI DLmodelURI;
+	protected IPath DLmodelPath;
+	private IGrammarAccess grammarAccess;
+	private TemplateLabelProvider labelProvider;
+	private FileOpener fileOpener;
+
+	protected ShowDiffPage mainPage;
+
+	public UpdateModelWizard(IGrammarAccess grammarAccess, TemplateLabelProvider labelProvider, FileOpener fileOpener,
+			IPath path) {
+		super();
+		this.DLmodelPath = path;
+		this.DLmodelURI = path.toFile().toURI();
+		setWindowTitle("Update TyphonDL model " + getDLModelName());
+		this.grammarAccess = grammarAccess;
+		this.labelProvider = labelProvider;
+		this.fileOpener = fileOpener;
 	}
 
-	public void setModelPath(URI uri) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addPages() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean canFinish() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void createPageControls(Composite pageContainer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
+	private String getDLModelName() {
+		return this.DLmodelPath.lastSegment().substring(0, this.DLmodelPath.lastSegment().lastIndexOf('.'));
 	}
 
 	@Override
-	public IWizardContainer getContainer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Image getDefaultPageImage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IDialogSettings getDialogSettings() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IWizardPage getNextPage(IWizardPage page) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IWizardPage getPage(String pageName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getPageCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public IWizardPage[] getPages() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IWizardPage getPreviousPage(IWizardPage page) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IWizardPage getStartingPage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public RGB getTitleBarColor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getWindowTitle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isHelpAvailable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean needsPreviousAndNextButtons() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean needsProgressMonitor() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean performCancel() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void setContainer(IWizardContainer wizardContainer) {
-		// TODO Auto-generated method stub
-		
+	protected ShowDiffPage createMainPage(String pageName) {
+		/*
+		 * TODO read container type and create array with just one entry: container type
+		 */
+		AbstractFileTemplate[] templates = null; 
+		mainPage = new ShowDiffPage(pageName, templates, selection, labelProvider, DLmodelURI);
+		super.mainPage = mainPage;
+		return mainPage;
 	}
 
 }
