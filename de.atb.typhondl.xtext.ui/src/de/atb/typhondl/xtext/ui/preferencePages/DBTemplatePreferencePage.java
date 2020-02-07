e071a40e27c31c78eb6f7786c6286a6686402c5d
flug <flug@atb-bremen.de>
2020-02-07 14:32:25 +0100

    seperate DB and DBType in Templates

---
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/preferencePages/DBTemplatePreferencePage.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package de.atb.typhondl.xtext.ui.preferencePages;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.templates.ContextTypeRegistry;
+import org.eclipse.jface.text.templates.persistence.TemplateStore;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.xtext.ui.editor.templates.XtextTemplatePreferencePage;
+
+import com.google.inject.Inject;
+
+/**
+ * @author flug
+ *
+ */
+public class DBTemplatePreferencePage extends XtextTemplatePreferencePage {
+
+	@Inject
+	public DBTemplatePreferencePage(IPreferenceStore preferenceStore, ContextTypeRegistry registry,
+			TemplateStore templateStore) {
+		super(preferenceStore, registry, templateStore);
+	}
+	
+	@Override
+	protected Control createContents(Composite ancestor) {
+		Control result = super.createContents(ancestor);
+		ancestor.layout();
+		super.getTableViewer().setContentProvider(new MyTemplateContentProvider("de.atb.typhondl.xtext.TyphonDL.DB"));
+		return result;
+	}
+
+}
