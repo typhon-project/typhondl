a1f02912bf280d2a36a93dde8f050ead3a8c6dd6
flug <flug@atb-bremen.de>
2020-02-07 16:14:39 +0100

    DBs have the DBType taken from the templates

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/utilities/PreferenceReader.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/utilities/PreferenceReader.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.templates.Template;
 import org.eclipse.jface.text.templates.TemplateBuffer;
 import org.eclipse.jface.text.templates.TemplateException;
@@ -24,16 +25,30 @@ import de.atb.typhondl.xtext.typhonDL.TyphonDLFactory;
 import de.atb.typhondl.xtext.ui.activator.Activator;
 import de.atb.typhondl.xtext.ui.creationWizard.ModelCreator;
 
+/**
+ * The PreferenceReader reads the templates defined in Eclipse Preferences ->
+ * TyphonDL -> DB Templates and DBType Templates for each metatype
+ * (relationaldb, documentdb, graphdb, keyvaluedb). Only DBTypes listed in the
+ * plugins polystore.properties can be used.
+ * 
+ * @author flug
+ *
+ */
 public class PreferenceReader {
 
 	/**
+	 * read all DBs fitting the <code>metatype</code> from the Eclipse Preferences
+	 * TyphonDL Template pages. Also adds the fitting DBType (also taken from the templates).
+	 * 
 	 * @param metatype \in {relationaldb, documentdb, graphdb, keyvaluedb}
 	 */
 	public static DBMS[] readDBs(String metatype) {
 		TemplateStore templateStore = Activator.getDefault().getInjector("de.atb.typhondl.xtext.TyphonDL")
 				.getInstance(TemplateStore.class);
-		Template[] templates = templateStore.getTemplates("de.atb.typhondl.xtext.TyphonDL.DB");
-		List<String> supportedTypes = new ArrayList<>();
+		// load the DB and DBType templates
+		Template[] dbTemplates = templateStore.getTemplates("de.atb.typhondl.xtext.TyphonDL.DB");
+		Template[] dbTypeTemplates = templateStore.getTemplates("de.atb.typhondl.xtext.TyphonDL.DBType");
+		// get polystore.properties to link the metatype to the actual DBMSs
 		String PATH_TO_PROPERTIES = "de/atb/typhondl/xtext/ui/properties/polystore.properties";
 		Properties properties = new Properties();
 		InputStream inStream = ModelCreator.class.getClassLoader().getResourceAsStream(PATH_TO_PROPERTIES);
@@ -42,25 +57,36 @@ public class PreferenceReader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		supportedTypes = Arrays.asList(((String) properties.get(metatype)).split(" "));
+		List<String> possibleTypes = Arrays.asList(((String) properties.get(metatype)).split(" "));
+		// get the parser to transalte the template pattern into a TyphonDL model
+		// instance
 		IParser parser = Activator.getDefault().getInjector("de.atb.typhondl.xtext.TyphonDL")
 				.getInstance(IParser.class);
+		// get all dbTypes that match the metatype
+		ArrayList<DBType> dbTypes = new ArrayList<>();
+		for (int i = 0; i < dbTypeTemplates.length; i++) {
+			TemplateBuffer buffer = getTemplateBuffer(dbTypeTemplates[i]);
+			DBType dbtype = getModelObject(TyphonDLFactory.eINSTANCE.createDBType(),
+					parser.parse(new StringReader(buffer.getString())));
+			if (possibleTypes.contains(dbtype.getName())) {
+				dbTypes.add(dbtype);
+			}
+		}
 		ArrayList<DBMS> dbmss = new ArrayList<>();
-		for (int i = 0; i < templates.length; i++) {
-			TemplateBuffer buffer = getTemplateBuffer(templates[i]);
+		for (int i = 0; i < dbTemplates.length; i++) {
+			TemplateBuffer buffer = getTemplateBuffer(dbTemplates[i]);
 			// for now the buffer variables do not contain the DBType
 			// parsed like this, the DBType is null
-			DB db = getDB(parser.parse(new StringReader(buffer.getString())));
+			DB db = getModelObject(TyphonDLFactory.eINSTANCE.createDB(),
+					parser.parse(new StringReader(buffer.getString())));
 			if (db != null) {
-				for (String supportedType : supportedTypes) {
-					if (buffer.getString().contains(supportedType)) {
-						DBType dbType = TyphonDLFactory.eINSTANCE.createDBType();
-						dbType.setName(supportedType);
-						db.setType(dbType);
+				for (DBType supportedType : dbTypes) {
+					if (buffer.getString().contains(supportedType.getName())) {
+						db.setType(supportedType);
 					}
 				}
 				if (db.getType() != null) {
-					dbmss.add(new DBMS(db.getType(), metatype, templates[i].getName()));
+					dbmss.add(new DBMS(db.getType(), metatype, dbTemplates[i].getName()));
 				} else {
 					// TODO error message, template is not well defined, only supported DBMS can be
 					// used and each database must have a type
@@ -72,6 +98,12 @@ public class PreferenceReader {
 		return dbmss.toArray(new DBMS[0]);
 	}
 
+	/**
+	 * 
+	 * @param template
+	 * @return the TemplateBuffer containing the translated template pattern as a
+	 *         String and an array of the template's variables
+	 */
 	private static TemplateBuffer getTemplateBuffer(Template template) {
 		TemplateTranslator templateTranslator = new TemplateTranslator();
 		TemplateBuffer buffer = null;
@@ -83,15 +115,23 @@ public class PreferenceReader {
 		return buffer;
 	}
 
-	private static DB getDB(IParseResult result) {
-		List<DB> dbs = ((DeploymentModel) result.getRootASTElement()).getElements().stream()
-				.filter(element -> DB.class.isInstance(element)).map(element -> (DB) element)
+	/**
+	 * Transforms the parsed <code>result</code> into a model object of given Type
+	 * <code>T</code>
+	 * 
+	 * @param <T>         the type of the wanted model object (here DB or DBType)
+	 * @param modelObject a dummy modelObject to pass the wanted Type
+	 * @param result      the parsed template pattern
+	 * @return the model object or <code>null</code> if the template does not
+	 *         contain exactly one definition
+	 */
+	private static <T extends EObject> T getModelObject(T modelObject, IParseResult result) {
+		List<T> elements = ((DeploymentModel) result.getRootASTElement()).getElements().stream()
+				.filter(element -> modelObject.getClass().isInstance(element)).map(element -> (T) element)
 				.collect(Collectors.toList());
-		if (dbs.size() != 1) {
-			return null; // there should only be one database in a template TODO warning!
+		if (elements.size() != 1) {
+			return null; // there should only be one model definition in a template TODO warning!
 		}
-		DB db = dbs.get(0);
-		return db;
+		return elements.get(0);
 	}
-
 }
