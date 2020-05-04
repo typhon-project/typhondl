aebf71622f34a20ff08167e2faf8ef3d4f3c7e42
flug <flug@atb-bremen.de>
2020-05-04 19:20:09 +0200

    made script better

--- de.vw.testing/wmDeployment_k8s_minikube/deploy.sh
+++ de.vw.testing/wmDeployment_k8s_minikube/deploy.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 kubeconfig="--kubeconfig=/home/flug/.kube/config"
 echo "Create Typhon namespace"
+kubectl create namespace typhon $kubeconfig
 if [ -n "$kubeconfig" ]; then
     echo "Using Cluster configuration file ${kubeconfig}"
 fi
-kubectl create namespace typhon $kubeconfig
 sleep 1
-echo "Deploy databases and upload models to metadata database (polystore-mongo)"
+echo "----------------------------------------------------------------------------"
+echo "Create databases"
 kubectl apply -n typhon -f databases.yaml $kubeconfig
 sleep 1
-echo "Wait for databases to complete startup"
+echo "----------------------------------------------------------------------------"
+echo "Wait for the models to be inserted into the metadata database"
 kubectl wait --for=condition=complete --timeout=300s -n typhon job.batch/insert-models $kubeconfig
+kubectl logs job/insert-models
+echo "----------------------------------------------------------------------------"
+echo "Wait for all databases to be ready"
+kubectl wait --for=condition=available --timeout=100s --all -n typhon deployments $kubeconfig
+echo "----------------------------------------------------------------------------"
 echo "Deploy Polystore"
 kubectl apply -n typhon -f polystore.yaml $kubeconfig
-
+echo "Wait for the API, UI and QL to be ready"
+kubectl wait --for=condition=available --timeout=300s --all -n typhon deployments $kubeconfig
+echo "----------------------------------------------------------------------------"
 echo "Polystore installation completed."
-echo "It may take a few minutes for all services to be up and running."
 exit 1
