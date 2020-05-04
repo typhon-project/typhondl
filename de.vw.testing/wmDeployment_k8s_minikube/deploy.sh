8c40aade3285f7354f3a18b40288305fbf7ec190
flug <flug@atb-bremen.de>
2020-05-04 18:34:29 +0200

    adjust example

--- de.vw.testing/wmDeployment_k8s_minikube/deploy.sh
+++ de.vw.testing/wmDeployment_k8s_minikube/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "Create Typhon namespace"
-kubectl create namespace typhon --kubeconfig=/home/flug/.kube/config
+kubectl create namespace typhon 
 sleep 1
 echo "Deploy databases and upload models to metadata database (polystore-mongo)"
-kubectl apply -n typhon -f databases.yaml --kubeconfig=/home/flug/.kube/config
+kubectl apply -n typhon -f databases.yaml 
 sleep 1
 echo "Wait for databases to complete startup"
-kubectl wait --for=condition=complete --timeout=300s -n typhon job.batch/insert-models --kubeconfig=/home/flug/.kube/config
+kubectl wait --for=condition=complete --timeout=300s -n typhon job.batch/insert-models 
 echo "Deploy Polystore"
-kubectl apply -n typhon -f polystore.yaml --kubeconfig=/home/flug/.kube/config
+kubectl apply -n typhon -f polystore.yaml 
+
 echo "Polystore installation completed."
 echo "It may take a few minutes for all services to be up and running."
 exit 1
