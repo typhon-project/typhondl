0738d608980065a5470e3aeca775a7ee5a1cc25e
flug <flug@atb-bremen.de>
2020-06-15 09:20:12 +0200

    unzip analytics zip, then delete it

--- de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/services/Services.java
+++ de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/services/Services.java
@@ -1,5 +1,6 @@
 package de.atb.typhondl.acceleo.services;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -18,6 +19,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
@@ -60,6 +63,8 @@ import de.atb.typhondl.xtext.typhonDL.TyphonDLFactory;
  *
  */
 public class Services {
+
+    private static final int BUFFER_SIZE = 4096;
 
     /**
      * Starts the script generation process, deletes old files
@@ -206,6 +211,7 @@ public class Services {
         InputStream input = null;
         OutputStream output = null;
         HttpURLConnection connection = null;
+        String analyticsZipPath = folder + "/analyticsKubernetes.zip";
         try {
             URL url = new URL("http://typhon.clmsuk.com/static/analyticsKubernetes.zip");
             connection = (HttpURLConnection) url.openConnection();
@@ -224,7 +230,7 @@ public class Services {
 
             // download the file
             input = connection.getInputStream();
-            output = new FileOutputStream(folder + "/analytics.zip");
+            output = new FileOutputStream(analyticsZipPath);
 
             byte data[] = new byte[4096];
             long total = 0;
@@ -244,10 +250,54 @@ public class Services {
             } catch (IOException ignored) {
                 System.out.println("EXCEPTION!");
             }
-
             if (connection != null)
                 connection.disconnect();
         }
+
+        unzip(analyticsZipPath, folder);
+    }
+
+    private static void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+        // delete zip
+        new File(zipFilePath).delete();
+    }
+
+    /**
+     * Extracts a zip entry (file entry)
+     * 
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
     }
 
     /**
