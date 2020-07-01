4ec54d0da1ee99e8f57a054266a9b3a4353b7161
scholze <g0Nz0347the3rds>
2020-07-01 12:48:02 +0200

    preserve original license

--- de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/Activator.java
+++ de.atb.typhondl.acceleo/src/de/atb/typhondl/acceleo/Activator.java
@@ -1,24 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package de.atb.typhondl.acceleo;
-
-/*-
- * #%L
- * de.atb.typhondl.acceleo
- * %%
- * Copyright (C) 2018 - 2020 ATB
- * %%
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- * 
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is
- * available at https://www.gnu.org/software/classpath/license.html.
- * 
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
- * #L%
- */
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
