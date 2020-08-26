db43dddb2f8f4fca54af11897ccb80fac7d2fb7c
flug <flug@atb-bremen.de>
2020-08-26 10:40:12 +0200

    add NLAE for realz

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/creationWizard/CreationMainPage.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/creationWizard/CreationMainPage.java
@@ -104,6 +104,7 @@ public class CreationMainPage extends MyWizardPage {
     private boolean useAnalytics;
     private boolean createScripts;
     private boolean analyticsContained;
+    private boolean useNLAE;
 
     private Text analyticsURIText;
 
@@ -123,6 +124,7 @@ public class CreationMainPage extends MyWizardPage {
         this.useAnalytics = false;
         this.createScripts = false;
         this.analyticsContained = true;
+        this.useNLAE = false;
     }
 
     @Override
@@ -148,8 +150,28 @@ public class CreationMainPage extends MyWizardPage {
     private void addGroups() {
         createMainGroup();
         createAnalyticsGroup();
+        createNLAEGroup();
         createConnectionGroup();
         createScalingGroup();
+    }
+
+    private void createNLAEGroup() {
+        Group mainGroup = new Group(main, SWT.READ_ONLY);
+        mainGroup.setLayout(new GridLayout(1, false));
+        mainGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+        mainGroup.setText("Typhon Natural Language Analysis Engine (NLAE)");
+
+        Button nlae = new Button(mainGroup, SWT.CHECK);
+        nlae.setText("Use Typhon NLAE (needs Docker Swarm)");
+        nlae.setSelection(useNLAE);
+        nlae.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                useNLAE = nlae.getSelection();
+                properties.setProperty(PropertiesService.POLYSTORE_USENLAE, Boolean.toString(useNLAE));
+            }
+        });
+
     }
 
     private void createScalingGroup() {
@@ -496,6 +518,10 @@ public class CreationMainPage extends MyWizardPage {
 
     public boolean getCreateScripts() {
         return createScripts;
+    }
+
+    public boolean getUseNLAE() {
+        return useNLAE;
     }
 
     /**
---
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/scriptGeneration/NLAEService.java
@@ -0,0 +1,94 @@
+package de.atb.typhondl.xtext.ui.scriptGeneration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import de.atb.typhondl.xtext.typhonDL.DeploymentModel;
+import de.atb.typhondl.xtext.typhonDL.Software;
+import de.atb.typhondl.xtext.typhonDL.TyphonDLFactory;
+import de.atb.typhondl.xtext.typhonDL.URI;
+import de.atb.typhondl.xtext.ui.modelUtils.SoftwareService;
+import de.atb.typhondl.xtext.ui.properties.PropertiesService;
+import de.atb.typhondl.xtext.ui.utilities.FileService;
+
+public class NLAEService {
+
+    private static final String NLAE_ZIP_FILENAME = "nlae.zip";
+    private static final String NLAE_ZIP_ADDRESS = "http://typhon.clmsuk.com/static/" + NLAE_ZIP_FILENAME;
+
+    public static DeploymentModel addNLAE(DeploymentModel model, Properties properties) {
+        Software nlae = SoftwareService.create(properties.getProperty(PropertiesService.NLAE_NAME), null);
+        nlae.setExternal(true);
+        URI uri = TyphonDLFactory.eINSTANCE.createURI();
+        uri.setValue(properties.getProperty(PropertiesService.NLAE_API_HOST) + ":"
+                + properties.getProperty(PropertiesService.NLAE_API_PORT));
+        nlae.setUri(uri);
+        model.getElements().add(nlae);
+        return model;
+    }
+
+    public static void addNLAEFiles(DeploymentModel model, String outputFolder, Properties properties) {
+        try {
+            String nlaeZipPath = outputFolder + File.separator + NLAE_ZIP_FILENAME;
+            InputStream input = FileService.downloadFiles(nlaeZipPath, NLAE_ZIP_ADDRESS, "NLAE");
+            final String nlaeFolder = nlaeZipPath.substring(0, nlaeZipPath.lastIndexOf('.'));
+            if (input != null) {
+                FileService.unzip(nlaeZipPath, nlaeFolder);
+            }
+            applyPropertiesToNLAEFiles(nlaeFolder, properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void applyPropertiesToNLAEFiles(String nlaePath, Properties properties) throws IOException {
+        Path flinkConfigPath = Paths.get(nlaePath + File.separator + "conf" + File.separator + "flink-conf.yaml");
+        HashMap<String, String> flinkPropertyMap = new HashMap<>();
+        flinkPropertyMap.put("jobmanager.heap.size:", PropertiesService.NLAE_JOBMANAGER_HEAPSIZE);
+        flinkPropertyMap.put("taskmanager.heap.size:", PropertiesService.NLAE_TASKMANAGER_HEAPSIZE);
+        flinkPropertyMap.put("taskmanager.numberOfTaskSlots:", PropertiesService.NLAE_TASKMANAGER_SLOTS);
+        flinkPropertyMap.put("parallelism.default:", PropertiesService.NLAE_PARALLELISM);
+        FileService.applyMapToFile(properties, flinkConfigPath, flinkPropertyMap);
+
+        Path nlaeDeploymentPath = Paths.get(nlaePath + File.separator + "nlae-compose.yml");
+        List<String> deploymentYAML = Files.readAllLines(nlaeDeploymentPath);
+        for (int i = 0; i < deploymentYAML.size(); i++) {
+            String line = deploymentYAML.get(i);
+            if (line.contains("8080")) {
+                deploymentYAML.set(i, line.replace("8080", properties.getProperty(PropertiesService.NLAE_API_PORT)));
+            }
+            if (line.contains("/path/to/models")) {
+                deploymentYAML.set(i,
+                        line.replace("/path/to/models", properties.getProperty(PropertiesService.NLAE_SHAREDVOLUME)));
+            }
+        }
+        int taskmanagerIndex = getTaskmanagerIndex(deploymentYAML);
+        for (int i = taskmanagerIndex; i < deploymentYAML.size(); i++) {
+            String line = deploymentYAML.get(i);
+            if (line.contains("replicas:")) {
+                deploymentYAML.set(i,
+                        line.replace("2", properties.getProperty(PropertiesService.NLAE_TASKMANAGER_REPLICAS)));
+            }
+        }
+        Files.write(nlaeDeploymentPath, deploymentYAML, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    private static int getTaskmanagerIndex(List<String> kafkaCluster) {
+        for (int i = 0; i < kafkaCluster.size(); i++) {
+            if (kafkaCluster.get(i).contains("taskmanager")) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+}
