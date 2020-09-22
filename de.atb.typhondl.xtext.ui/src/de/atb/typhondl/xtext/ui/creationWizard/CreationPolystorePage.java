17ad6585460cc7960eca7198be1a4f8669ba8391
flug <flug@atb-bremen.de>
2020-09-22 22:34:34 +0200

    add resource fields to polystore page

--- de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/creationWizard/CreationPolystorePage.java
+++ de.atb.typhondl.xtext.ui/src/de/atb/typhondl/xtext/ui/creationWizard/CreationPolystorePage.java
@@ -1,5 +1,7 @@
 package de.atb.typhondl.xtext.ui.creationWizard;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import org.eclipse.swt.SWT;
@@ -12,6 +14,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import de.atb.typhondl.xtext.ui.properties.PropertiesService;
+import de.atb.typhondl.xtext.ui.utilities.InputField;
 import de.atb.typhondl.xtext.ui.utilities.SupportedTechnologies;
 
 public class CreationPolystorePage extends MyWizardPage {
@@ -39,8 +42,10 @@ public class CreationPolystorePage extends MyWizardPage {
         main.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
         main.setLayout(new GridLayout(1, false));
 
-        createQLGroup();
-        createAPIGroup();
+        ResourceEditor editor = new ResourceEditor();
+        createQLGroup(editor.getResourceFieldsQL());
+        createAPIGroup(editor.getResourceFieldsAPI());
+        createUIGroup();
 
         if (chosenTechnology == SupportedTechnologies.Kubernetes) {
             createLoggingAndMonitoringGroup();
@@ -53,29 +58,56 @@ public class CreationPolystorePage extends MyWizardPage {
 
     }
 
-    private void createAPIGroup() {
-
-        Group apiGroup = new Group(main, SWT.READ_ONLY);
-        apiGroup.setLayout(new GridLayout(2, false));
-        apiGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-        apiGroup.setText("API settings");
-
-        createScalingFields(apiGroup, "API", PropertiesService.API_REPLICAS);
+    private void createUIGroup() {
+        Group uiGroup = createGroup("UI settings");
+        createPublishedPortFields(uiGroup, PropertiesService.UI_PUBLISHEDPORT);
     }
 
-    private void createQLGroup() {
+    private void createAPIGroup(List<InputField> list) {
+        Group apiGroup = createGroup("API settings");
+        GridData gridData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
+        createScalingFields(apiGroup, "API", PropertiesService.API_REPLICAS);
+        createPublishedPortFields(apiGroup, PropertiesService.API_PUBLISHEDPORT);
+        for (InputField inputField : list) {
+            createResourceFields(apiGroup, gridData, inputField);
+        }
+    }
 
-        Group qlGroup = new Group(main, SWT.READ_ONLY);
-        qlGroup.setLayout(new GridLayout(2, false));
-        qlGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-        qlGroup.setText("QL Settings");
+    private void createPublishedPortFields(Group group, String property) {
+        // TODO Auto-generated method stub
 
+    }
+
+    private void createQLGroup(List<InputField> list) {
+        Group qlGroup = createGroup("QL Settings");
+        GridData gridData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
         createScalingFields(qlGroup, "QL Server", PropertiesService.QLSERVER_REPLICAS);
+        for (InputField inputField : list) {
+            createResourceFields(qlGroup, gridData, inputField);
+        }
+    }
+
+    private void createResourceFields(Group qlGroup, GridData gridData, InputField inputField) {
+        // TODO put in hidden composite, activate with checkbox
+        new Label(qlGroup, NONE).setText(inputField.label);
+        Text text = new Text(qlGroup, SWT.BORDER);
+        text.setText(properties.getProperty(inputField.propertyName));
+        text.setLayoutData(gridData);
+        text.addModifyListener(e -> {
+            properties.setProperty(inputField.propertyName, text.getText());
+        });
     }
 
     private void createLoggingAndMonitoringGroup() {
         // TODO Auto-generated method stub
+    }
 
+    private Group createGroup(String name) {
+        Group group = new Group(main, SWT.READ_ONLY);
+        group.setLayout(new GridLayout(2, false));
+        group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+        group.setText(name);
+        return group;
     }
 
     private void createScalingFields(Group group, String component, String property) {
@@ -87,4 +119,25 @@ public class CreationPolystorePage extends MyWizardPage {
         rep.addModifyListener(e -> properties.setProperty(property, rep.getText()));
     }
 
+    private class ResourceEditor {
+        public List<InputField> resourceFieldsQL = Arrays.asList(
+                new InputField("limit.memory: ", PropertiesService.QLSERVER_LIMIT_MEMORY),
+                new InputField("limit.cpu: ", PropertiesService.QLSERVER_LIMIT_CPU),
+                new InputField("reservation.memory: ", PropertiesService.QLSERVER_RESERVATION_MEMORY),
+                new InputField("reservation.cpu: ", PropertiesService.QLSERVER_RESERVATION_CPU));
+
+        public List<InputField> resourceFieldsAPI = Arrays.asList(
+                new InputField("limit.memory: ", PropertiesService.API_LIMIT_MEMORY),
+                new InputField("limit.cpu: ", PropertiesService.API_LIMIT_CPU),
+                new InputField("reservation.memory: ", PropertiesService.API_RESERVATION_MEMORY),
+                new InputField("reservation.cpu: ", PropertiesService.API_RESERVATION_CPU));
+
+        public List<InputField> getResourceFieldsQL() {
+            return resourceFieldsQL;
+        }
+
+        public List<InputField> getResourceFieldsAPI() {
+            return resourceFieldsAPI;
+        }
+    }
 }
